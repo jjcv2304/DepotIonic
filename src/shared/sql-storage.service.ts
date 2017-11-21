5f9cd1bbe95931f7267a43e88af272f014c23058
Juan <jjcv2304@hotmail.com>
2017-11-21 22:14:59 +0100

    Adding SQL access and seed method for ShelfType and ItemType

--- src/shared/sql-storage.service.ts
+++ src/shared/sql-storage.service.ts
@@ -1,8 +1,14 @@
 import {Injectable} from '@angular/core';
 import {SQLite, SQLiteObject} from '@ionic-native/sqlite';
-import {IShelfType} from "../models/shelf-type.model";
+
 import {Observable} from "rxjs/Observable";
+import 'rxjs/add/observable/forkJoin';
 import 'rxjs/add/observable/fromPromise';
+
+
+import {IShelfType} from "../models/shelf-type.model";
+import {IItemType} from "../models/item-type.model";
+import {IShelf} from "../models/shelf.model";
 
 @Injectable()
 export class SqlStorage {
@@ -15,10 +21,12 @@ export class SqlStorage {
     return this.sqlite.create({name: 'depot.db', location: 'default'}).then(db => {
       this.db = db;
 
-      this.createTableItemIfNotExists();
-      this.createTableItemTypeIfNotExists();
-      this.createTableShelfIfNotExists();
       this.createTableShelfTypeIfNotExists();
+      this.createTableShelfIfNotExists();
+      this.createTableItemTypeIfNotExists();
+      this.createTableItemIfNotExists();
+      //this.seedDatabaseIfEmpty.call(this);
+      this.seedDatabaseIfEmpty();
 
     }, (error) => {
       console.log("ERROR: ", error);
@@ -28,88 +36,157 @@ export class SqlStorage {
 
   private createTableItemIfNotExists() {
     this.db.executeSql('CREATE TABLE IF NOT EXISTS item (id integer primary key, name text, description text, expirationDate integer, width integer, height integer, typeId integer, shelfId integer)', [])
-      .then(data => {
-        console.info('Table item created ' + data);
-      })
-      .catch(e => console.error(e));
+      .then().catch(e => console.error(e));
   };
 
   private createTableItemTypeIfNotExists() {
     this.db.executeSql('CREATE TABLE IF NOT EXISTS itemType (id integer primary key, name text, description text)', [])
-      .then(data => {
-        console.info('Table itemType created ' + data);
-      })
-      .catch(e => console.error(e));
+      .then().catch(e => console.error(e));
   };
 
   private createTableShelfIfNotExists() {
-    this.db.executeSql('CREATE TABLE IF NOT EXISTS shelf (id integer primary key, name text, description text, width integer, height integer, deepth integer, location text, shelfParent integer, typeId integer)', [])
-      .then(data => {
-        console.info('Table shelf created ' + data);
-      })
-      .catch(e => console.error(e));
+    this.db.executeSql('CREATE TABLE IF NOT EXISTS shelf (id integer primary key, name text, description text, width integer, height integer, depth integer, location text, shelfParent integer, typeId integer)', [])
+      .then().catch(e => console.error(e));
   };
 
   private createTableShelfTypeIfNotExists() {
     this.db.executeSql('CREATE TABLE IF NOT EXISTS shelfType (id integer primary key, name text, description text)', [])
-      .then(data => {
-        console.info('Table shelfType created ' + data);
-      })
-      .catch(e => console.error(e));
+      .then().catch(e => console.error(e));
+  };
+
+  private seedDatabaseIfEmpty() {
+
+    Observable.forkJoin(
+      this.getShelfTypes(),
+      this.getItemTypes(),
+      function (shelfTypes, itemTypes) { return shelfTypes.length + itemTypes.length; }
+    ).subscribe( (totalRecordsFound) => {
+        if (totalRecordsFound == 0) {
+          const SHELVESTYPES: IShelfType[] = [
+            {
+              id: 1,
+              name: "Fridge",
+              description: "Cool shelf"
+            },
+            {
+              id: 2,
+              name: "Freezer",
+              description: "Very Cool shelf"
+            },
+            {
+              id: 3,
+              name: "Shelf",
+              description: "Shelf, 25C"
+            }
+          ];
+          for (let i = 0; i < SHELVESTYPES.length; i++) {
+            this.addShelfType(SHELVESTYPES[i]);
+          }
+          const ITEMTYPES: IItemType[] = [
+            {
+              id: 1,
+              name: 'Alimento',
+              description: 'Se puede comer'
+            },
+            {
+              id: 2,
+              name: 'Ropa',
+              description: 'Ropa en general'
+            },
+            {
+              id: 11,
+              name: 'Verdura',
+              description: 'Es una verdura o vegetal'
+            },
+            {
+              id: 12,
+              name: 'Legumbre',
+              description: 'Es legumbre'
+            },
+            {
+              id: 13,
+              name: 'Listo para comer',
+              description: 'Es un alimento/plato que esta listo para ser consumido sin mas elaboracion'
+            },
+            {
+              id: 21,
+              name: 'Ropa vestir',
+              description: 'Ropa para trabajar o salir con los amigos'
+            },
+            {
+              id: 22,
+              name: 'Ropa deporte',
+              description: 'Ropa para practicar deporte'
+            },
+          ];
+          for (let i = 0; i < ITEMTYPES.length; i++) {
+            this.addItemType(ITEMTYPES[i]);
+          }
+        }
+    });
   };
 
   getShelfTypes(): Observable<IShelfType[]> {
     return Observable.fromPromise(
-      this.db.executeSql('SELECT id, name, description FROM shelfType', []).then(data => {
-        let results: IShelfType[] = [];
-        for (let i = 0; i < data.rows.length; i++) {
-          // results.push(JSON.parse(data.rows.item(i)));
-          results.push(data.rows.item(i));
-        }
-        return results;
-      }).catch(function () {
+      this.db.executeSql('SELECT id, name, description FROM shelfType', [])
+        .then(data => {
+          let results: IShelfType[] = [];
+          for (let i = 0; i < data.rows.length; i++) {
+            results.push(data.rows.item(i));
+          }
+          return results;
+        }).catch(function () {
         return null;
       })
     );
   };
-
-  addShelfType(shelfType: IShelfType) {
-    return this.db.executeSql('insert into shelfType(id, name, description) values (?, ?, ? )', [shelfType.id, shelfType.name, shelfType.description])
-      .then(data => {
-        if (data.rows.length > 0) {
-          return JSON.parse(data.rows.item(0).value);
-        }
-      });
+  addShelfType(shelfType: IShelfType): Promise<any> {
+    return this.db.executeSql('insert into shelfType(id, name, description) values (?, ?, ? )', [shelfType.id, shelfType.name, shelfType.description]);
+  };
+  deleteShelfType(id: number) {
+    return this.db.executeSql('delete from shelfType where id = ? ', [id]);
   }
 
-  // getAll() {
-  //   return this.db.executeSql('SELECT key, value FROM kv', []).then(data => {
-  //     let results = [];
-  //     for (let i = 0; i < data.rows.length; i++) {
-  //       results.push(JSON.parse(data.rows.item(i).value));
-  //     }
-  //     return results;
-  //   });
-  // }
-  //
-  // get(key: string) {
-  //   return this.db.executeSql('select key, value from kv where key = ? limit 1', [key]).then(data => {
-  //     if (data.rows.length > 0) {
-  //       return JSON.parse(data.rows.item(0).value);
-  //     }
-  //   });
-  // }
-  //
-  // remove(key: string) {
-  //   return this.db.executeSql('delete from kv where key = ?', [key]);
-  // }
-  //
-  // set(key: string, value: string) {
-  //   return this.db.executeSql('insert or replace into kv(key, value) values (?, ?)', [key, value]).then(data => {
-  //     if (data.rows.length > 0) {
-  //       return JSON.parse(data.rows.item(0).value);
-  //     }
-  //   });
-  // }
-  //
+  getItemTypes(): Observable<IItemType[]> {
+    return Observable.fromPromise(
+      this.db.executeSql('SELECT id, name, description FROM itemType', [])
+        .then(data => {
+          let results: IItemType[] = [];
+          for (let i = 0; i < data.rows.length; i++) {
+            results.push(data.rows.item(i));
+          }
+          return results;
+        }).catch(function () {
+        return null;
+      })
+    );
+  };
+  addItemType(iItemType: IItemType): Promise<any> {
+    return this.db.executeSql('insert into itemType(id, name, description) values (?, ?, ? )', [iItemType.id, iItemType.name, iItemType.description]);
+  };
+  deleteItemType(id: number) {
+    return this.db.executeSql('delete from itemType where id = ? ', [id]);
+  }
+
+  getShelves(): Observable<IShelf[]> {
+    return Observable.fromPromise(
+      this.db.executeSql('SELECT id, name, description, width, height, depth, location, shelfParent, typeId FROM shelf', [])
+        .then(data => {
+          let results: IShelf[] = [];
+          for (let i = 0; i < data.rows.length; i++) {
+            results.push(data.rows.item(i));
+          }
+          return results;
+        }).catch(function () {
+        return null;
+      })
+    );
+  };
+  addShelf(shelf: IShelf): Promise<any> {
+    return this.db.executeSql('insert into shelfType(id, name, description, width, height, depth, location, shelfParent, typeId) values (?, ?, ?, ?, ?, ?, ?, ?, ? )', [shelf.id, shelf.name, shelf.description, shelf.width, shelf.height, shelf.depth, shelf.location, shelf.shelfParent, shelf.type.id]);
+  };
+  deleteShelf(id: number) {
+    return this.db.executeSql('delete from shelf where id = ? ', [id]);
+  }
+
 }
